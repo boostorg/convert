770794d4d559bf8b6e63ff303a4e23a21c1d122b
Mike <Wasowski>
2014-02-09 19:04:10 +1100

    Updating docs

--- boost/convert/parameters.hpp
+++ boost/convert/parameters.hpp
@@ -1,13 +1,6 @@
-/// \file boost/convert.hpp
-/// Public interface to the Boost.Convert framework.
-//
 // Boost.Convert library
 //
-// Copyright (c) 2009-2011 Vladimir Batov.
-// Many thanks to Robert Stewart, Scott McMurray, Andrey Semashev, Dave Abrahams,
-// Hartmut Kaiser, Anders Dalvander, Andrzej Krzemienski, Andrew Troschinetz and
-// all the Boosters participated in the related discussions and the review.
-// In many ways the library has been influenced and shaped by them.
+// Copyright (c) 2009-2014 Vladimir Batov.
 // Use, modification and distribution are subject to the Boost Software License,
 // Version 1.0. See http://www.boost.org/LICENSE_1_0.txt.
 
