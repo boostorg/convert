044f30f8e5a092c0adf095fc0c3ac66635f934ec
VB <vb.mail.247@gmail.com>
2014-06-03 14:47:22 +1000

    added example dir

--- example/example.hpp
+++ example/example.hpp
@@ -3,11 +3,8 @@
 
 struct example
 {
-<<<<<<< HEAD
     static void getting_started();
 
-=======
->>>>>>> 8ac3f69797fcbe91813d2d1704951f22d9661aa3
     struct algorithm
     {
         static void strings_to_ints_simple();
