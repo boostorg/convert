595a4b746c3c7170eea3cb4f0da07a4ca7e1abb0
Vladimir Batov <vb.mail.247@gmail.com>
2014-06-19 20:35:57 +1000

    cleanup

--- test/algorithms.cpp
+++ test/algorithms.cpp
@@ -6,39 +6,7 @@
 #include "./test.hpp"
 #include "../example/example.hpp"
 
-static
-void
-user_type_to_strs()
-{
-    boost::array<change, 3>             chgs1 = {{ change::no, change::up, change::dn }};
-    boost::array<change::value_type, 3> chgs2 = {{ change::no, change::up, change::dn }};
-    std::vector<std::string>            strs1;
-    std::vector<std::string>            strs2;
-    std::vector<std::string>            strs3;
-    boost::cnv::cstringstream           cnv;
-
-    std::transform(chgs1.begin(), chgs1.end(), std::back_inserter(strs1), boost::convert<std::string, change>(boost::cref(cnv)));
-    std::transform(chgs2.begin(), chgs2.end(), std::back_inserter(strs2), boost::convert<std::string, change>(boost::cref(cnv)));
-    std::transform(chgs2.begin(), chgs2.end(), std::back_inserter(strs3), boost::convert<std::string, change::value_type>(boost::cref(cnv)));
-
-    BOOST_TEST(strs1.size() == 3);
-    BOOST_TEST(strs1[0] == "no");
-    BOOST_TEST(strs1[1] == "up");
-    BOOST_TEST(strs1[2] == "dn");
-
-//    BOOST_TEST(strs2.size() == 3);
-//    BOOST_TEST(strs2[0] == "no");
-//    BOOST_TEST(strs2[1] == "up");
-//    BOOST_TEST(strs2[2] == "dn");
-
-    BOOST_TEST(strs3.size() == 3);
-    BOOST_TEST(strs3[0] == "0");
-    BOOST_TEST(strs3[1] == "1");
-    BOOST_TEST(strs3[2] == "2");
-}
-
 void
 test::cnv::algorithms()
 {
-    user_type_to_strs();
 }
--- test/user_type.cpp
+++ test/user_type.cpp
@@ -8,8 +8,9 @@
 using std::string;
 using std::wstring;
 
+static
 void
-test::cnv::user_type()
+test_user_type()
 {
     boost::cnv::cstringstream cnv; // stringstream-based char converter
 
@@ -36,4 +37,42 @@ test::cnv::user_type()
     BOOST_TEST(dn_dir3 == direction::dn);
     BOOST_TEST(dn_dir4 == direction::dn);
     BOOST_TEST(!up_dir4); // Failed conversion
+}
+
+static
+void
+test_algorithms()
+{
+    boost::array<change, 3>             chgs1 = {{ change::no, change::up, change::dn }};
+    boost::array<change::value_type, 3> chgs2 = {{ change::no, change::up, change::dn }};
+    std::vector<std::string>            strs1;
+    std::vector<std::string>            strs2;
+    std::vector<std::string>            strs3;
+    boost::cnv::cstringstream           cnv;
+
+    std::transform(chgs1.begin(), chgs1.end(), std::back_inserter(strs1), boost::convert<std::string, change>(boost::cref(cnv)));
+    std::transform(chgs2.begin(), chgs2.end(), std::back_inserter(strs2), boost::convert<std::string, change>(boost::cref(cnv)));
+    std::transform(chgs2.begin(), chgs2.end(), std::back_inserter(strs3), boost::convert<std::string, change::value_type>(boost::cref(cnv)));
+
+    BOOST_TEST(strs1.size() == 3);
+    BOOST_TEST(strs1[0] == "no");
+    BOOST_TEST(strs1[1] == "up");
+    BOOST_TEST(strs1[2] == "dn");
+
+    BOOST_TEST(strs2.size() == 3);
+    BOOST_TEST(strs2[0] == "no");
+    BOOST_TEST(strs2[1] == "up");
+    BOOST_TEST(strs2[2] == "dn");
+
+    BOOST_TEST(strs3.size() == 3);
+    BOOST_TEST(strs3[0] == "0");
+    BOOST_TEST(strs3[1] == "1");
+    BOOST_TEST(strs3[2] == "2");
+}
+
+void
+test::cnv::user_type()
+{
+    test_user_type();
+    test_algorithms();
 }
