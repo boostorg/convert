e9d24dd857d1cec382e492cf5fa7ff2615aba000
Vladimir Batov <vb.mail.247@gmail.com>
2019-04-26 21:27:36 +1000

    corrected 'comparison between signed and unsigned integer'

--- include/boost/convert/strtol.hpp
+++ include/boost/convert/strtol.hpp
@@ -158,15 +158,16 @@ template<typename string_type, typename out_type>
 void
 boost::cnv::strtol::str_to_i(cnv::range<string_type> range, boost::optional<out_type>& result_out) const
 {
-    typedef typename boost::make_unsigned<out_type>::type unsigned_type;
-    typedef cnv::range<string_type>                          range_type;
-    typedef typename range_type::iterator                      iterator;
+    using     uint_type = unsigned int;
+    using unsigned_type = typename boost::make_unsigned<out_type>::type;
+    using    range_type = cnv::range<string_type>;
+    using      iterator = typename range_type::iterator;
 
     iterator             s = range.begin();
-    unsigned int        ch = *s;
+    uint_type           ch = *s;
     bool const is_negative = ch == '-' ? (ch = *++s, true) : ch == '+' ? (ch = *++s, false) : false;
     bool const is_unsigned = boost::is_same<out_type, unsigned_type>::value;
-    int               base = int(base_);
+    uint_type         base = uint_type(base_);
 
     /**/ if (is_negative && is_unsigned) return;
     else if ((base == 0 || base == 16) && ch == '0' && (*++s == 'x' || *s == 'X')) ++s, base = 16;
@@ -175,7 +176,7 @@ boost::cnv::strtol::str_to_i(cnv::range<string_type> range, boost::optional<out_
     unsigned_type const    max = (std::numeric_limits<out_type>::max)();
     unsigned_type const   umax = max + (is_negative ? 1 : 0);
     unsigned_type const cutoff = umax / base;
-    unsigned int  const cutlim = umax % base;
+    uint_type     const cutlim = umax % base;
     unsigned_type       result = 0;
 
     for (; s != range.sentry(); ++s)
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 BOOST_ROOT = $(PRJROOT)/boost
 
-cflags = -O3 -I./include -I$(BOOST_ROOT) -Wno-unused-variable -Wno-uninitialized -Wno-sign-compare -Wno-deprecated-declarations 
+cflags = -O3 -I./include -I$(BOOST_ROOT) -Wno-unused-variable -Wno-uninitialized -Wno-deprecated-declarations 
 lflags = -L$(BOOST_ROOT)/stage/lib -lboost_timer -lboost_chrono -lboost_system
 
 target_1  = convert-test-callable           test/callable.cpp
